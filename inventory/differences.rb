ae624d086343cec3ff6c15a4d2ab536c0acb6a90
Abraham Nieva <a_nieva@hotmail.com>
2015-05-16 11:20:27 -0500

    Chapter 5 fix chomp bug and verify results are ok (V 7 - 8)

--- inventory/differences.rb
+++ inventory/differences.rb
@@ -4,10 +4,13 @@ unless ARGV.length == 2
 end
 
 def inventory_from(filename)
+  # open the file
   inventory=File.open(filename)
+  # convert to lowercase and delete trailing \n
   downcased=inventory.collect do | line |
-    line.downcase
+    line.chomp.downcase
   end
+  # reject all filenames containing temp and recycler
   downcased.reject do | line |
     boring?(line)
   end    
@@ -24,17 +27,22 @@ old_inventory = inventory_from(ARGV[0])
 new_inventory = inventory_from(ARGV[1])
 
 x = (new_inventory - old_inventory).length
-
-puts "The following #{x} files have been added:"
+# Excercise: add number of added files
+puts "The following #{x} file(s) have been added:"
 puts new_inventory - old_inventory
 
 y = (old_inventory - new_inventory).length
 
 puts ""
-puts "The following #{y}  files have been deleted:"
+# Excercise: add number of deleted files
+puts "The following #{y}  file(s) have been deleted:"
 puts old_inventory - new_inventory
 
-x = new_inventory.length - y
+x = new_inventory.length - x
+
+y = old_inventory.length - y 
 
 puts ""
+# Excercise: add number of unchanged files
 puts "Unchanged files: #{x} "
+puts "Verification of unchanged files: #{y} "
