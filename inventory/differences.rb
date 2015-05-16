adae42130a61afba7823096a97adb5c1d0e26c93
Abraham Nieva <a_nieva@hotmail.com>
2015-05-16 11:03:41 -0500

    Chapter 3 excercise 3

--- inventory/differences.rb
+++ inventory/differences.rb
@@ -1,9 +1,18 @@
 old_inventory = File.open('old-inventory.txt' ).readlines
 new_inventory = File.open('new-inventory.txt' ).readlines
 
-puts "The following files have been added:"
+x = (new_inventory - old_inventory).length
+
+puts "The following #{x} files have been added:"
 puts new_inventory - old_inventory
 
+y = (old_inventory - new_inventory).length
+
 puts ""
-puts "The following files have been deleted:"
+puts "The following #{y}  files have been deleted:"
 puts old_inventory - new_inventory
+
+x = new_inventory.length - y
+
+puts ""
+puts "Unchanged files: #{x} "
